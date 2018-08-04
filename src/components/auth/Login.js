ab21171de87e51e6ba30615e6727424d6316ed5f
Sean Olson <seanton213@gmail.com>
2018-08-03 17:17:04 -0700

    Finished Login. Need to test auth

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,28 +1,91 @@
 import React, {Component} from 'react';
 import { Container, Row, Collapse } from 'reactstrap';
 import '../../App.css';
-// import ImageBox from "../../utils/ImageBox/ImageBox";
-// import NavHeader from '../../utils/NavHeader/NavHeader';
-// import './HomePage.css';
 import * as firebase from "firebase";
+import { SignUpLink } from './SignUp';
 
+const LoginPage = ({ history }) =>
+    <div>
+        <h1>SignIn</h1>
+        <LoginForm history={history} />
+        <SignUpLink />
+    </div>
 
-export default class Login extends Component {
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapse: false,
-      targetId: null
-    };
-  }
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null,
+};
 
-  render() {
-    return (
-      <div>
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
 
-        <p>Login</p>
-      </div>
-    );
-  }
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = (event) => {
+        const {
+            email,
+            password,
+        } = this.state;
+
+        const {
+            history,
+        } = this.props;
+
+        firebase.auth.doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                history.push(routes.HOME);
+            })
+            .catch(error => {
+                this.setState(byPropKey('error', error));
+            });
+
+        event.preventDefault();
+    }
+
+    render() {
+        const {
+            email,
+            password,
+            error,
+        } = this.state;
+
+        const isInvalid =
+            password === '' ||
+            email === '';
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    value={email}
+                    onChange={event => this.setState(byPropKey('email', event.target.value))}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    value={password}
+                    onChange={event => this.setState(byPropKey('password', event.target.value))}
+                    type="password"
+                    placeholder="Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign In
+                </button>
+
+                { error && <p>{error.message}</p> }
+            </form>
+        );
+    }
 }
+
+export {
+  LoginForm,
+};
+
